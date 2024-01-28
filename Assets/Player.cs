1f43ba8516037ad886fb6e501a9afc855fb2b610
Milton <milton_pinheiro@outlook.com>
2024-01-28 10:41:17 -0400

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -102,14 +102,14 @@ public class Player : NetworkBehaviour
         RecoverServer(pers, spawn);
     }
 
-    [ObserversRpc]
+    [ServerRpc(RequireOwnership = false)]
     public void RecoverServer(NetworkObject pers, Vector3 spawn){
         print("a "+spawn);
         print(pers.OwnerId);
         RecoverClient(pers, spawn);
     }
 
-    [ServerRpc(RequireOwnership = false)]
+    [ObserversRpc]
     public void RecoverClient(NetworkObject pers, Vector3 spawn){
         print("b "+spawn);
         pers.transform.position = spawn;
